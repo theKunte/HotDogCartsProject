11deef84deeba908727572ea2846e6c33633a5a3
Benjamin-Jenne <jenne.ben.e@gmail.com>
2020-03-03 21:25:48 -0800

    I add the adminRouter to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,27 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var vendorRouter = require('./routes/vendor');
-var incomingOrdersRouter = require('./routes/incomingOrders')
-var vendorLocationRouter = require('./routes/vendorLocation');
-
-
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var vendorRouter = require("./routes/vendor");
+var incomingOrdersRouter = require("./routes/incomingOrders");
+var vendorLocationRouter = require("./routes/vendorLocation");
+var adminRouter = require("./routes/admin");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/vendor', vendorRouter);
-app.use('/incomingOrders', incomingOrdersRouter);
-app.use('/vendorLocation', vendorLocationRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/vendor", vendorRouter);
+app.use("/incomingOrders", incomingOrdersRouter);
+app.use("/vendorLocation", vendorLocationRouter);
 
 module.exports = app;
