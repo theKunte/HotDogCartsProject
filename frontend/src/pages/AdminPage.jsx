602ee95ff4448f996d7c1a0af118ff23f940d2a7
Benjamin-Jenne <jenne.ben.e@gmail.com>
2020-03-07 11:35:59 -0800

    I added limited database connectivity

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,27 +1,51 @@
-import React from 'react';
-import { Component } from 'react';
+import React from "react";
+import { Component } from "react";
 // import VendorRenderBox from './VendorRenderBox';
-import AdminSideNav from '../components/AdminSideNav';
-import { Container, Row, Col } from 'react-bootstrap';
+import AdminSideNav from "../components/AdminSideNav";
+import { Container, Row, Col } from "react-bootstrap";
 
 class AdminPage extends Component {
-    render() {
-        return (
-            <Container fluid>
-                <Row>
-                    <Col xs='4' sm='3' md='3' lg='2' xl='2' style={{ paddingLeft: '0' }}>
-                        <AdminSideNav />
-                    </Col>
-                    <Col xs='8' sm='9' md='9' lg='10' xl='10' style={{ paddingTop: '75px'}}>
-                        <h6>Active list of vendors:</h6>
-                        <hr></hr>
-                        {/* <AdminRenderBox /> */}
-
-                    </Col>
-                </Row>
-             </Container>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      vendors: []
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:3000/admin/vendors/")
+      .then(res => res.json())
+      .then(vendors => this.setState({ vendors }));
+  }
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col xs="4" sm="3" md="3" lg="2" xl="2" style={{ paddingLeft: "0" }}>
+            <AdminSideNav />
+          </Col>
+          <Col
+            xs="8"
+            sm="9"
+            md="9"
+            lg="10"
+            xl="10"
+            style={{ paddingTop: "75px" }}
+          >
+            <h6> Active list of vendors: </h6>
+            <ul>
+              {this.state.vendors.map(vendor => (
+                <li key={vendor.LocationID}>
+                  {" "}
+                  {vendor.Address} {vendor.Availability}
+                </li>
+              ))}
+            </ul>
+            {/* <AdminRenderBox /> */}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default AdminPage;
