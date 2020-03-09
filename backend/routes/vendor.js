4c87b19229c9ae113f70f6acb7185fb70b260981
theKunte <jennykunte90@gmail.com>
2020-03-08 22:49:45 -0700

    added Logout Page for Vendor and added more functunality to Incoming - Not working correctly

--- backend/routes/vendor.js
+++ backend/routes/vendor.js
@@ -1,8 +1,33 @@
-const express = require('express');
-const router = express.Router();
-const vendorJson = require('../data/vendor.json');
-const vendorLocationJson = require('../data/vendorLocation.json');
+var express = require("express");
+var router = express.Router();
+let vendorJson = require("../data/vendor.json");
+let orderJson = require("../data/incomingOrders.json");
+let menuitemsJson = require("../data/menutitems.json");
+let db = require("../data/db");
 
+//shows cartlocation of vendor
+router.get("vendors", function(req,res,next){
+    let sql = "Call ShowLocation";
+    db.query(sql,(err,rows) => {
+        if(err)throw err;
+        else{
+            console.log(rows[0].length);
+        }
+        res.send(rows[0]);
+    });
+});
+
+// This should send bacl orders when vendor selects incoming orders
+router.get("/orders", function(req,res,next){
+    let sql = "Call ShowOrder";
+    db.query(sql, (err,rows)=>{
+        if (err) throw err;
+        else{
+            console.lof(rows[0].length);
+        }
+        res.send(rows[0]);
+    });
+});
 
 //Get All Vendor
 router.get('/', function (req, res) {
