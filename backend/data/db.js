f64e981404c172f45dd70a6bf854f04aeb2ce987
Benjamin-Jenne <jenne.ben.e@gmail.com>
2020-03-14 20:28:52 -0700

    I changed my password

--- backend/data/db.js
+++ backend/data/db.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 var db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "enter your password",
+  password: "@Castle27",
   database: "HotDogDatabase"
 });
 
