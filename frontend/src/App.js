aa1c9f4adfbf87ac55e2730f2754fe39204f601c
Chris <christopher_hogue@hotmail.com>
2020-03-15 21:31:59 -0700

    Fix issues on app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,71 +11,33 @@ import Footer from './components/layouts/Footer';
 import Home from './pages/Home';
 import CartLocations from './pages/CartLocations';
 import MenuPage from './pages/MenuPage';
-import AdminPage from './pages/AdminPage';
+// import AdminPage from './pages/AdminPage';
 import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+// import RegisterPage from './pages/RegisterPage';
 
-=======
-import React from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import VendorLogOut from "./pages/VendorLogOut";
-import VendorPage from "./pages/VendorPage";
-import VendorMenu from "./pages/VendorMenu";
-import VendorIncomingOrder from "./pages/VendorIncomingOrder";
-import VendorCartLocation from "./pages/VendorCartLocation";
-import Navigation from "./components/layouts/Navigation";
-import Footer from "./components/layouts/Footer";
-import Home from "./pages/Home";
-import CartLocations from "./pages/CartLocations";
-import MenuPage from "./pages/MenuPage";
+
+
 import Cart from "./pages/Cart";
 import AdminVendor from "./pages/AdminVendor";
-import LoginPage from "./pages/LoginPage";
 import AdminAddCart from "./pages/AdminAddCart";
 import AdminLog from "./pages/AdminLog";
 import AdminIncomingOrder from "./pages/AdminIncomingOrder";
 import AdminMenu from "./pages/AdminMenu";
 import { ProductProvider } from "../src/data/context";
->>>>>>> 22f1cc557b550564c289583b6a0d5b4d023f5eae
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-
-      <Router>
-        
-          <Route path="/vendor" cmponent ={VendorPage} />
-          <Route path="/vendorin" component={VendorIncomingOrder} />
-          <Route path="/vendormenu" component ={VendorMenu} />
-          <Router path="/vendorlocation" component={VendorCartLocation} />
-          
-      </Router>
-      <Navigation />
-      <Router>
-        
-=======
       <ProductProvider>
         <Navigation />
         <Router>
->>>>>>> 22f1cc557b550564c289583b6a0d5b4d023f5eae
           <Route exact path="/" component={Home} />
           <Route path="/cart-locations" component={CartLocations} />
           <Route path="/menu" component={MenuPage} />
           <Route path="/cart" component={Cart} />
           <Route path="/adminvendor" component={AdminVendor} />
           <Route path="/vendor" component={VendorPage} />
-<<<<<<< HEAD
-          <Route path="/registerPage" component={RegisterPage} />
-          <Route path="/loginPage" component={LoginPage} />
-        
-      </Router>
-      <Footer />
-    </div> 
-=======
-          <Route path="/vendorLogout" component={VendorLogOut} />
+          {/* <Route path="/vendorLogout" component={VendorLogOut} /> */}
           <Route path="/login" component={LoginPage} />
           <Route path="/vendor" cmponent={VendorPage} />
           <Route path="/vendorin" component={VendorIncomingOrder} />
@@ -90,7 +51,6 @@ function App() {
         <Footer />
       </ProductProvider>
     </div>
->>>>>>> 22f1cc557b550564c289583b6a0d5b4d023f5eae
   );
 }
 
